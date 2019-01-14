48e362a199415d409818e4b4fd476a7ebea46265
Mick van Gelderen <mickvangelderen@gmail.com>
2019-01-14 13:53:29 +0100

    Initial implementation
    Not sure what version of rustc you need to use this.
    Not sure if we need nightly.
    Not sure if we can get rid of `#![feature(proc_macro_hygiene)]` in client code,
    don't want to use proc-macro-hack.

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+extern crate proc_macro;
+
+#[proc_macro]
+pub fn static_cstr(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let literal = syn::parse_macro_input!(input as syn::LitStr);
+
+    let cstring = match std::ffi::CString::new(literal.value()) {
+        Ok(x) => x,
+        Err(err) => {
+            return syn::Error::new(proc_macro::Span::call_site().into(), err)
+                .to_compile_error()
+                .into();
+        }
+    };
+
+    let expr = syn::LitByteStr::new(
+        cstring.as_bytes_with_nul(),
+        proc_macro::Span::call_site().into(),
+    );
+
+    quote::quote!({
+        union Cast {
+            from_byte_slice: &'static [u8],
+            to_cstr: &'static std::ffi::CStr,
+        }
+
+        unsafe {
+            Cast { from_byte_slice: #expr }.to_cstr
+        }
+    })
+    .into()
+}
